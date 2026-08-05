b15821334eabdca386f52f02d5b19c287d48d1b2
forge-compliance <gphewett@gmail.com>
2026-08-05 11:38:40 +0100

    Delete assets/config.js

--- assets/config.js
+++
@@ -1,5 +0,0 @@
-window.LIMEWOOD_CONFIG = {
-  supabaseUrl: 'https://sswedojvkqvoqmwhfmkj.supabase.co',
-  supabasePublishableKey: 'sb_publishable_BoOHg11kfg6FB3xhQHW-dw_yal1q1Yu',
-  storageBucket: 'asset-files'
-};

c904317b620bd4862c41a6eb373024aeb0e344db
forge-compliance <gphewett@gmail.com>
2026-08-05 11:30:42 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+window.LIMEWOOD_CONFIG = {
+  supabaseUrl: 'https://sswedojvkqvoqmwhfmkj.supabase.co',
+  supabasePublishableKey: 'sb_publishable_BoOHg11kfg6FB3xhQHW-dw_yal1q1Yu',
+  storageBucket: 'asset-files'
+};

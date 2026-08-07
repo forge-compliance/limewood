a3ecd8bd0543816ff10ae89cfd2710a71f45ec49
forge-compliance <gphewett@gmail.com>
2026-08-07 23:12:08 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='limewood-engineering-v1';
-const CORE=['./','index.html','styles.css','app.js','manifest.json','data/database.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));

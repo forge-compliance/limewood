813517a27382460d631e00e05365f96202ba128f
forge-compliance <gphewett@gmail.com>
2026-08-29 13:31:30 +0100

    Switch SOP cards to stable editor

--- assets/config.js
+++ assets/config.js
@@ -13,11 +13,10 @@ window.LIMEWOOD_BMS = {baseUrl:'https://192.168.170.100',readOnly:true,hostName:
  add('link',{rel:'stylesheet',href:'/assets/navigation-v9.css?v=20260829-1'});
  add('script',{src:'/assets/navigation-v9.js?v=20260829-1'});
  if(/^\/(?:index\.html)?$/i.test(location.pathname)){
-   add('script',{src:'/assets/sop-builder.js?v=20260829-5'});
+   add('script',{src:'/assets/sop-editor-stable.js?v=20260829-1'});
    add('script',{src:'/assets/sop-pdf-download.js?v=20260829-4'});
-   add('script',{src:'/assets/sop-card-controls.js?v=20260829-1'});
+   add('script',{src:'/assets/sop-card-controls.js?v=20260829-2'});
  }
- add('script',{src:'/assets/sop-photo-links.js?v=20260829-5'});
  if(/\/maintenance-dashboard\.html$/i.test(location.pathname))add('script',{src:'/assets/maintenance-asset-picker.js?v=20260828-1'});
  if(/\/photo-inbox\.html$/i.test(location.pathname))add('script',{src:'/assets/photo-inbox-batch.js?v=20260829-2'});
 })();

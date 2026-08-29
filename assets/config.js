24c118164c77bbddc247363ecb15b15e6c8365f2
forge-compliance <gphewett@gmail.com>
2026-08-29 13:11:01 +0100

    Load SOP photo picker v4

--- assets/config.js
+++ assets/config.js
@@ -16,7 +16,7 @@ window.LIMEWOOD_BMS = {baseUrl:'https://192.168.170.100',readOnly:true,hostName:
    add('script',{src:'/assets/sop-builder.js?v=20260829-2'});
    add('script',{src:'/assets/sop-pdf-download.js?v=20260829-2'});
  }
- add('script',{src:'/assets/sop-photo-links.js?v=20260829-3'});
+ add('script',{src:'/assets/sop-photo-links.js?v=20260829-4'});
  if(/\/maintenance-dashboard\.html$/i.test(location.pathname))add('script',{src:'/assets/maintenance-asset-picker.js?v=20260828-1'});
  if(/\/photo-inbox\.html$/i.test(location.pathname))add('script',{src:'/assets/photo-inbox-batch.js?v=20260829-2'});
 })();

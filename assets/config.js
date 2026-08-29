bc72107a2bb10e9148840890311e1c55fc5a6746
forge-compliance <gphewett@gmail.com>
2026-08-29 13:08:56 +0100

    Load redesigned SOP PDF generator

--- assets/config.js
+++ assets/config.js
@@ -14,7 +14,7 @@ window.LIMEWOOD_BMS = {baseUrl:'https://192.168.170.100',readOnly:true,hostName:
  add('script',{src:'/assets/navigation-v9.js?v=20260829-1'});
  if(/^\/(?:index\.html)?$/i.test(location.pathname)){
    add('script',{src:'/assets/sop-builder.js?v=20260829-2'});
-   add('script',{src:'/assets/sop-pdf-download.js?v=20260829-1'});
+   add('script',{src:'/assets/sop-pdf-download.js?v=20260829-2'});
  }
  add('script',{src:'/assets/sop-photo-links.js?v=20260829-3'});
  if(/\/maintenance-dashboard\.html$/i.test(location.pathname))add('script',{src:'/assets/maintenance-asset-picker.js?v=20260828-1'});

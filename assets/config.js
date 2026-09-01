91b800fbf24dc266e475f93d5d70b9d6c342ae63
forge-compliance <gphewett@gmail.com>
2026-09-01 11:32:17 +0100

    Bump compact Photo Inbox redesign

--- assets/config.js
+++ assets/config.js
@@ -29,7 +29,7 @@ window.LIMEWOOD_BMS = {
   }
   if(/\/photo-inbox\.html$/i.test(location.pathname)){
     const photoBatch=document.createElement('script'); photoBatch.src='/assets/photo-inbox-batch.js?v=20260830-3'; document.head.appendChild(photoBatch);
-    const photoInboxRedesign=document.createElement('link'); photoInboxRedesign.rel='stylesheet'; photoInboxRedesign.href='/assets/photo-inbox-redesign.css?v=20260901-1'; document.head.appendChild(photoInboxRedesign);
+    const photoInboxRedesign=document.createElement('link'); photoInboxRedesign.rel='stylesheet'; photoInboxRedesign.href='/assets/photo-inbox-redesign.css?v=20260901-2'; document.head.appendChild(photoInboxRedesign);
   }
   if(/\/electrical-distribution\.html$/i.test(location.pathname)){
     const electricalLayout=document.createElement('link'); electricalLayout.rel='stylesheet'; electricalLayout.href='/assets/electrical-distribution-newlayout.css?v=20260830-3'; document.head.appendChild(electricalLayout);
